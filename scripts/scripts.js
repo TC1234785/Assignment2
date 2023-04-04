4519f98c739a34ca882d40b991d6cdc324d11b2a
Your Name <youremail@yourdomain.com>
2023-04-04 05:08:39 -0700

    Now the dark theme button is complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+/* Dark Theme Button functionality */
+
 const button = document.querySelector("#Dark_Theme");
 
 function changeDarkTheme()
@@ -5,7 +7,7 @@ function changeDarkTheme()
     document.body.classList.toggle('darkThemeMain');
     document.querySelector('aside').classList.toggle('darkThemeAside')
     document.querySelector('html').classList.toggle('darkThemeText')
-    document.querySelector('.navbar a').classList.toggle('darkThemeText') /*why doesn't this work????*/
+    document.querySelector('.navbar a').classList.toggle('darkThemeText')
     if (button.textContent=== "Dark Theme"){
         button.textContent= 'Light Theme'
     }
@@ -14,3 +16,4 @@ function changeDarkTheme()
     } 
 }
 button.addEventListener("click", changeDarkTheme);
+
