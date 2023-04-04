7c27474b4952ccc91c977efaf321d77de91485a7
Your Name <youremail@yourdomain.com>
2023-04-04 10:10:43 -0700

    Cancel button functionality complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,32 @@
 /* Dark Theme Button functionality */
 
-const button = document.querySelector("#Dark_Theme");
+const buttonDarkTheme = document.querySelector("#Dark_Theme");
 
 function changeDarkTheme()
 {
     document.body.classList.toggle('darkThemeMain');
-    document.querySelector('aside').classList.toggle('darkThemeAside')
-    document.querySelector('html').classList.toggle('darkThemeText')
-    document.querySelector('.navbar a').classList.toggle('darkThemeText')
-    if (button.textContent=== "Dark Theme"){
-        button.textContent= 'Light Theme'
+    document.querySelector('aside').classList.toggle('darkThemeAside');
+    document.querySelector('html').classList.toggle('darkThemeText');
+    if (buttonDarkTheme.textContent=== "Dark Theme"){
+        buttonDarkTheme.textContent= 'Light Theme';
     }
     else {
-        button.textContent= "Dark Theme"
+        buttonDarkTheme.textContent= "Dark Theme";
     } 
 }
-button.addEventListener("click", changeDarkTheme);
+buttonDarkTheme.addEventListener("click", changeDarkTheme);
+
+/* Cancel button functionality */
+const buttonCancel = document.querySelector("#Cancel")
+
+function cancelButton()
+{
+    document.querySelector('.bottom_buttons').classList.add("hidden")
+    document.querySelector('#note').classList.add("hidden")
+}
+buttonCancel.addEventListener('click', cancelButton)
+
+
+
+
 
