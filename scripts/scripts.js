95fce6672b2801ee9dc694ac0230d2f2a1742b88
Your Name <youremail@yourdomain.com>
2023-04-05 15:40:47 -0700

    corrected selectors for text area to work with classList

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,7 @@ function changeDarkTheme()
     document.body.classList.toggle('darkThemeMain');
     document.querySelector('aside').classList.toggle('darkThemeAside');
     document.querySelector('html').classList.toggle('darkThemeText');
+    document.querySelector('.note').classList.toggle('darkThemeAside');
     if (buttonDarkTheme.textContent=== "Dark Theme"){
         buttonDarkTheme.textContent= 'Light Theme';
     }
@@ -22,19 +23,29 @@ const buttonCancel = document.querySelector("#Cancel")
 function cancelButton()
 {
     document.querySelector('.bottom_buttons').classList.add("hidden")
-    document.querySelector('#note').classList.add("hidden")
+    document.querySelector('.note').classList.add("hidden")
 }
 buttonCancel.addEventListener('click', cancelButton)
 
 /* New Note functionality */
 const newNoteButton = document.querySelector("#New_Note")
-const textAreaPlaceholder = document.querySelector("#note")
+const textAreaPlaceholder = document.querySelector(".note")
 function newNote()
 {
-    document.querySelector('.bottom_buttons').classList.remove("hidden");
-    document.querySelector('#note').classList.remove("hidden");
-    textAreaPlaceholder.value = "";
+
+    
+    if(document.querySelector('.bottom_buttons').classList.contains("hidden")){
+        document.querySelector('.bottom_buttons').classList.remove("hidden");
+        textAreaPlaceholder.classList.remove("hidden");
+    }
+    else{
+        textAreaPlaceholder.value = "";
+    }
 }
 newNoteButton.addEventListener('click', newNote)
+
+/*Declare notesArray*/
+const notesArray = [{title:"note one", body:"this is my first note"}, {title:"note two", body:"this is my second note"}]
+
 
 
