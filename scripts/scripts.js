510248cc3825fdc21d346a9680521246284497dd
Your Name <youremail@yourdomain.com>
2023-04-06 05:25:48 -0700

    complete save button functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,15 @@ const buttonDarkTheme = document.querySelector("#Dark_Theme");
 const buttonCancel = document.querySelector("#Cancel");
 const bottomButtons = document.querySelector('.bottom_buttons');
 const newNoteButton = document.querySelector("#New_Note")
+const saveButton = document.querySelector("#Save");
+const newLi = document.createElement('li')
+const userText = document.querySelector('textarea')
+const listNotes = document.querySelector('ul')
+
+
+
+/*Declare notesArray*/
+const notesArray = [{title:"note one", body:"this is my first note"}, {title:"note two", body:"this is my second note"}]
 
 /* Dark Theme Button functionality */
 function changeDarkTheme()
@@ -33,8 +42,7 @@ function cancelButton()
 }
 buttonCancel.addEventListener('click', cancelButton)
 
-/* New Note functionality */
-
+/* New Note button functionality */
 function newNote()
 {
     if(bottomButtons.classList.contains("hidden")){
@@ -47,9 +55,22 @@ function newNote()
 }
 newNoteButton.addEventListener('click', newNote)
 
-/*Declare notesArray*/
-const notesArray = [{title:"note one", body:"this is my first note"}, {title:"note two", body:"this is my second note"}]
-
-
-
+/*Save button functionality*/
+function saveNote(){
+    let noteTitle = prompt("What is the title for the note?");
+    if(noteTitle === ""){
+        alert("You did not input anything. A title cannot be empty")
+    }
+    else{
+        while (notesArray.find(note=> note.title === noteTitle)){
+            noteTitle = prompt('That title already exists. Try a different title')
+        };
+        newLi.textContent = noteTitle;
+        let noteObject = {title: noteTitle, body: note.value};
+        notesArray.push(noteObject);
+        listNotes.appendChild(newLi);
+        
+    }
+}
+saveButton.addEventListener('click', saveNote)
 
