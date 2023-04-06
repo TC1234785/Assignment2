3f0dde3a9e2ab9be171c65be0e8a94f95e53f1b1
Your Name <youremail@yourdomain.com>
2023-04-06 04:26:17 -0700

    removed most redundancies in query selectors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,19 @@
-/* Dark Theme Button functionality */
-
+/* Check all selectors*/
+const aside = document.querySelector('aside');
+const html = document.querySelector('html');
+const note = document.querySelector('.note');
 const buttonDarkTheme = document.querySelector("#Dark_Theme");
+const buttonCancel = document.querySelector("#Cancel");
+const bottomButtons = document.querySelector('.bottom_buttons');
+const newNoteButton = document.querySelector("#New_Note")
 
+/* Dark Theme Button functionality */
 function changeDarkTheme()
 {
     document.body.classList.toggle('darkThemeMain');
-    document.querySelector('aside').classList.toggle('darkThemeAside');
-    document.querySelector('html').classList.toggle('darkThemeText');
-    document.querySelector('.note').classList.toggle('darkThemeAside');
+    aside.classList.toggle('darkThemeAside');
+    html.classList.toggle('darkThemeText');
+    note.classList.toggle('darkThemeAside');
     if (buttonDarkTheme.textContent=== "Dark Theme"){
         buttonDarkTheme.textContent= 'Light Theme';
     }
@@ -18,34 +24,32 @@ function changeDarkTheme()
 buttonDarkTheme.addEventListener("click", changeDarkTheme);
 
 /* Cancel button functionality */
-const buttonCancel = document.querySelector("#Cancel")
+
 
 function cancelButton()
 {
-    document.querySelector('.bottom_buttons').classList.add("hidden")
-    document.querySelector('.note').classList.add("hidden")
+    bottomButtons.classList.add("hidden")
+    note.classList.add("hidden")
 }
 buttonCancel.addEventListener('click', cancelButton)
 
 /* New Note functionality */
-const newNoteButton = document.querySelector("#New_Note")
-const textAreaPlaceholder = document.querySelector(".note")
+
 function newNote()
 {
-
-    
-    if(document.querySelector('.bottom_buttons').classList.contains("hidden")){
-        document.querySelector('.bottom_buttons').classList.remove("hidden");
-        textAreaPlaceholder.classList.remove("hidden");
+    if(bottomButtons.classList.contains("hidden")){
+       bottomButtons.classList.remove("hidden");
+        note.classList.remove("hidden");
     }
     else{
-        textAreaPlaceholder.value = "";
+        note.value = "";
     }
 }
 newNoteButton.addEventListener('click', newNote)
 
 /*Declare notesArray*/
 const notesArray = [{title:"note one", body:"this is my first note"}, {title:"note two", body:"this is my second note"}]
+
 
 
 
